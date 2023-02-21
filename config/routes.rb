9b63f11c1c2dbbdcc2d088ff0517bc1272ec5392
Syed Sadiq Ali <syed.ali0902992@gmail.com>
2023-02-21 13:55:35 +0000

    Added index to Blog application

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :blogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+ root "blogs#index"
 end
--- db/migrate/20230221132755_create_blogs.rb
+++
@@ -1,8 +0,0 @@
-class CreateBlogs < ActiveRecord::Migration[7.0]
-  def change
-    create_table :blogs do |t|
-
-      t.timestamps
-    end
-  end
-end
