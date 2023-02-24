ec72aa18894642a9e464e4d8b06f34c001fb7e17
Syed Sadiq Ali <syed.ali0902992@gmail.com>
2023-02-24 14:02:40 +0000

    Added a flash message when record is saved

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,10 @@ class BlogsController < ApplicationController
   end
 
   def create
-    blog = Blog.new(title: params[:title], description: params[:description])
-    blog.save
+    @blog = Blog.new(title: params[:title], description: params[:description])
+    if @blog.save
+      flash[:alert]= "Saved the record successfully"
+      redirect_to root_path
+    end
   end
 end
