2b56544e405fea9da83c94887f7f25d58a15b80e
Shashin Dayanand <shashin.dayanand@thoughtworks.com>
2023-03-01 16:45:00 +0000

    Display all blogs in a table

--- test/controllers/blogs_controller_test.rb
+++ test/controllers/blogs_controller_test.rb
@@ -1,13 +1,21 @@
 require "test_helper"
-class BlogsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
 
-  test "should return all the records from the database" do
+class BlogsControllerTest < ActionDispatch::IntegrationTest
+
+  test "title should be correct" do
     blog = Blog.all
     get blogs_url
     assert_response :success
-    assert_select "h1", "#{blog.count}"
+    assert_select "h1", "Blogs"
+  end
+
+  test "should display correct number of blog" do
+    blog = Blog.all
+    get blogs_url
+    assert_select "tbody" do |elements|
+      elements.each do |element|
+        assert_select element, "td", blog.count
+      end
+    end
   end
 end
