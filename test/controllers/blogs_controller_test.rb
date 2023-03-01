311ee5b0a7c1d0502a65304f55e40d1368791384
Shashin Dayanand <shashin.dayanand@thoughtworks.com>
2023-03-01 16:56:07 +0000

    Updated test

--- test/controllers/blogs_controller_test.rb
+++ test/controllers/blogs_controller_test.rb
@@ -11,11 +11,10 @@ class BlogsControllerTest < ActionDispatch::IntegrationTest
 
   test "should display correct number of blog" do
     blog = Blog.all
+    Blog.create(title: 'test 4839', description: "another blog")
     get blogs_url
     assert_select "tbody" do |elements|
-      elements.each do |element|
-        assert_select element, "td", blog.count
-      end
+      assert_select elements, "td", blog.count
     end
   end
 end
