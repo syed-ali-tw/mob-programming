6ec4558b556cab555465fbc58e24e8e2b5572144
Syed Sadiq Ali <syed.ali0902992@gmail.com>
2023-03-03 15:45:51 +0000

    Added blog table headers

--- test/controllers/blogs_controller_test.rb
+++ test/controllers/blogs_controller_test.rb
@@ -10,11 +10,20 @@ class BlogsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should display correct number of blog" do
-    blog = Blog.all
-    Blog.create(title: 'test 4839', description: "another blog")
     get blogs_url
     assert_select "tbody" do |elements|
       assert_select elements, "td", blog.count
+    end
+  end
+
+  test "should validate that headers are correct for blog table" do
+
+    get blogs_url
+    assert_select "th", { count: 3 }
+    assert_select "thead" do
+      assert_select "th", "title"
+      assert_select "th", "description"
+      assert_select "th", "actions"
     end
   end
 end
