df9896c7c461868c26de498a2293f6ed4dc94b11
Syed Sadiq Ali <syed.ali0902992@gmail.com>
2023-03-09 16:44:50 +0000

    WIP

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,7 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+//= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/table
+//= require govuk_publishing_components/components/links
+//= require govuk_publishing_components/components/dependencies
--- config/initializers/govuk_publishing_components.rb
+++ config/initializers/govuk_publishing_components.rb
@@ -1,5 +1,5 @@
 GovukPublishingComponents.configure do |c|
   c.component_guide_title = "My component guide"
-  # c.application_stylesheet = "custom_stylesheet" # Defaults to "application"
-  # c.application_javascript = "custom_javascript" # Defaults to "application"
+  c.application_stylesheet = "custom_stylesheet" # Defaults to "application"
+  c.application_javascript = "custom_javascript" # Defaults to "application"
 end
