f975efb167f3244ae521249f8de5ff598ed33bbb
Syed Sadiq Ali <syed.ali0902992@gmail.com>
2023-03-09 23:46:08 +0000

    working code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,6 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/table
 //= require govuk_publishing_components/components/links
 //= require govuk_publishing_components/components/dependencies
+//= require govuk_publishing_components/components/action_links
