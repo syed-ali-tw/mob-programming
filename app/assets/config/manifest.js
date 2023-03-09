7dd603e4414dc24b8c585fc5d7da71f4a22a6b39
Syed Sadiq Ali <syed.ali0902992@gmail.com>
2023-03-09 18:58:04 +0000

    component working

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,5 @@
 //= link_tree ../images
 //= link_tree ../builds
+//= link application.scss
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import * as bootstrap from "bootstrap"
+// import "@hotwired/turbo-rails"
+// import "./controllers"
+// import * as bootstrap from "bootstrap"
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/table
 //= require govuk_publishing_components/components/links
