2482fc4209b86b8a28d1e292d85aec14f3c3f8de
farah1987 <farah.mahmood2018@gmail.com>
2023-03-22 16:56:53 +0000

    added test for Edit Blog page

--- test/system/blogs_page_test.rb
+++ test/system/blogs_page_test.rb
@@ -52,8 +52,8 @@ class ActionDispatch::IntegrationTest
     end
     puts page.current_path
     assert has_current_path? "/blogs/298486374/edit"
-    # assert find('h1.title').text == 'Edit Blog'
-    assert_text('Edit Blog')
+    assert find('h1.govuk-heading-xl').text == 'Edit Blog'
+    #assert_text('Edit Blog')
   end
 
   def within_table_row(position)
